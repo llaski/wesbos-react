6b8fb520084774e4371213524eead7c3a0390ac1
Larry Laski <larry.laski@gmail.com>
2015-11-13 13:04:22 -0500

    lesson 13

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,14 @@ var App = React.createClass({
         };
     },
 
+    addToOrder: function(key) {
+        this.state.order[key] = this.state.order[key] + 1 || 1;
+
+        this.setState({
+            order: this.state.order
+        });
+    },
+
     addFish: function(fish) {
         var timestamp = (new Date()).getTime();
 
@@ -54,7 +62,7 @@ var App = React.createClass({
 
     renderFish: function(key) {
         return (
-            <Fish key={key} index={key} details={this.state.fishes[key]} />
+            <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
         );
     }
 });
@@ -62,6 +70,8 @@ var App = React.createClass({
 var Fish = React.createClass({
     render: function() {
         var details = this.props.details;
+        var isAvailable = (details.status === 'available' ? true : false);
+        var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
 
         return (
             <li className="menu-fish">
@@ -71,8 +81,13 @@ var Fish = React.createClass({
                     <span className="price">{helpers.formatPrice(details.price)}</span>
                 </h3>
                 <p>{details.desc}</p>
+                <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
             </li>
         );
+    },
+
+    onButtonClick: function() {
+        this.props.addToOrder(this.props.index);
     }
 });
 //Add fish form
